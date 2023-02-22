50e4d278d377f451f1c95cdf7cef734935840201
Vadim Andreev <vadimcola@mail.ru>
2023-02-22 23:24:32 +0700

    функция форматирования даты

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from utils.utils import load_data
-
+from utils.utils import load_data, transaction_date
 
 BANK_STATEMENT_URL = "https://api.npoint.io/7e0408563a1ef990bc71"
 
-print(load_data(BANK_STATEMENT_URL))
+transaction = load_data(BANK_STATEMENT_URL)
+
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 
 def load_data(path):
@@ -9,3 +10,15 @@ def load_data(path):
     data = requests.get(path)
     data_ = data.json()
     return data_
+
+
+def transaction_date(list_transaction: list) -> str:
+    '''
+    форматирование даты в нужный вариант
+    :param list_transaction: список транзакций
+    :return: выдает дату в формате ДД.ММ.ГГГГ
+    '''
+    date = list_transaction['date'][0:10]
+    pre_format_date = time.strptime(date, "%Y-%m-%d")
+    format_date = time.strftime("%d.%m.%Y", pre_format_date)
+    return format_date
