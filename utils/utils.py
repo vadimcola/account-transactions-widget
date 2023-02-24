ab45a6f485416e2db1b833bdf0fd42dc6211d8a2
Vadim Andreev <vadimcola@mail.ru>
2023-02-24 18:44:48 +0700

    функция форматирования даты

--- utils/utils.py
+++ utils/utils.py
@@ -22,3 +22,27 @@ def transaction_date(list_transaction: list) -> str:
     pre_format_date = time.strptime(date, "%Y-%m-%d")
     format_date = time.strftime("%d.%m.%Y", pre_format_date)
     return format_date
+
+
+def payment_invoice(list_transaction: list) -> str:
+    """
+    функция выводит с какого счета была произведена оплата
+    :param list_transaction: список транзакция
+    :return: вывод в зашифрованном виде номер карты или счета показывается
+    не польностью
+    """
+    payment = list_transaction['from']
+    if len(payment.split()) == 3:
+        account_number = payment.split()[2]
+        number_output = f'{" ".join(payment.split()[0:2])} {account_number[:4]}' \
+                        f' {account_number[4:6]}** **** {account_number[-4:]} '
+    elif len(payment.split()) == 2:
+        account_number = payment.split()[1]
+        if len(payment.split()[1]) == 16:
+            number_output = f'{payment.split()[0]} {account_number[:4]}' \
+                            f' {account_number[4:6]}** **** {account_number[-4:]}'
+        elif len(payment.split()[1]) == 20:
+            number_output = f'{payment.split()[0]} {account_number[:4]}' \
+                            f' {account_number[4:6]}** **** **** {account_number[-4:]}'
+
+    return number_output
