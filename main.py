e63b3ee2760ad4aceff153a3964e41a2f81d857c
Vadim Andreev <vadimcola@mail.ru>
2023-02-25 10:33:12 +0700

    Исправил основной код

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from operator import itemgetter
 BANK_STATEMENT_URL = "https://api.npoint.io/7e0408563a1ef990bc71"
 
 # делаем сортировку списка словорей по определеным данным а также выводим первые пять словорей
-transaction = sorted(load_data(BANK_STATEMENT_URL), key=itemgetter('date'), reverse=True)[0:6]
+transaction = sorted(load_data(BANK_STATEMENT_URL), key=itemgetter('date'), reverse=True)
+
+counter = 0
 
 # запускаем цикл перебераем словори в списке
 for list_transaction in transaction:
@@ -16,17 +18,24 @@ for list_transaction in transaction:
     if list_transaction['state'] == 'CANCELED':
         continue
 
-    # если транзакция было открытием вклада то выводим сообщение с открытием вклада
-    elif list_transaction['description'] == "Открытие вклада":
-        print(f"{transaction_date(list_transaction)} {list_transaction['description']}\n"
-              f"Вклад открыт -> {payment_to(list_transaction)}\n"
-              f"{amount_currency(list_transaction)}")
+    if list_transaction is None:
+        continue
 
-    # если была оплата выводим сообщение с какого на какой счет
+    if counter < 5:
+        counter += 1
+        # если транзакция было открытием вклада то выводим сообщение с открытием вклада
+        if list_transaction['description'] == "Открытие вклада":
+            print(f"{transaction_date(list_transaction)} {list_transaction['description']}\n"
+                  f"Вклад открыт -> {payment_to(list_transaction)}\n"
+                  f"{amount_currency(list_transaction)}")
+
+        # если была оплата выводим сообщение с какого на какой счет
+        else:
+            print(f"{transaction_date(list_transaction)} {list_transaction['description']}\n"
+                  f"{payment_invoice(list_transaction)} -> {payment_to(list_transaction)}\n"
+                  f"{amount_currency(list_transaction)}")
+
+        # добавляем один пробел между каждым выводом
+        print()
     else:
-        print(f"{transaction_date(list_transaction)} {list_transaction['description']}\n"
-              f"{payment_invoice(list_transaction)} -> {payment_to(list_transaction)}\n"
-              f"{amount_currency(list_transaction)}")
-
-    # добавляем один пробел между каждым выводом
-    print()
+        break
